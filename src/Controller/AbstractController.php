b0d59fc2833a456b2c24c32e2e195d76dd2f8e7f
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-05-21 11:33:01 +0200

    Create AbstractController with two methods : one to render Twig template and another to redirect

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+abstract class AbstractController
+{
+    /**
+     * Render twig template.
+     */
+    public function render(string $template, array $data = [])
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('./templates/');
+        $twig = new \Twig\Environment($loader, ['debug' => true]);
+        $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+        echo $twig->render($template, $data);
+    }
+
+    /**
+     * Redirect method.
+     */
+    public function redirect(string $path, int $http_code = 302): void
+    {
+        header("Location: {$path}", true, $http_code);
+    }
+}
