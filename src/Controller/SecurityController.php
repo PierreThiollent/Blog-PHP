df24a39563d3e88164317df60a333482416796c4
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-04 16:26:52 +0200

    Create SecurityController with one method to register new user and another to confirm user account

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Helpers;
+use App\Hydrator;
+use App\Repository\UserRepository;
+
+class SecurityController extends AbstractController
+{
+    private UserRepository $repository;
+    private Hydrator $hydrator;
+
+    public function __construct()
+    {
+        $this->repository = new UserRepository();
+        $this->hydrator = new Hydrator();
+    }
+
+    /**
+     * Register new user
+     *
+     * @route /inscription
+     * @return void
+     */
+    public function register()
+    {
+        if (!empty($_POST)) {
+            $user = new User();
+
+            $this->hydrator->hydrate($user, $_POST);
+            $user->setPassword(password_hash($_POST['password'], PASSWORD_DEFAULT));
+
+            $token = Helpers::generateToken();
+            $user->setConfirmationToken($token);
+
+            if ($this->repository->userExist($user)) {
+                return $this->render('register.html.twig', [
+                    'error' => 'Un utilisateur avec cet email existe déjà. Vous pouvez vous <a href="/connexion">connecter</a> ou réinitialiser votre mot de passe si vous l\'avez oublié.'
+                ]);
+            }
+
+            $user_id = $this->repository->addUser($user);
+
+            if ($user_id) {
+                $user->setId($user_id);
+
+                mail(
+                    $user->getEmail(),
+                    'Confirmation de votre compte',
+                    "Pour confirmer cotre compte, veuillez cliquer sur le lien suivant.
+                    http://blog-php.local/confirmation/$user_id-$token"
+                );
+
+                return $this->render(
+                    'register.html.twig',
+                    ['message' => 'Un mail de confirmation vient de vous être envoyé. Veuillez cliquer sur le lien afin de confirmer votre comtpe.']
+                );
+            }
+        }
+
+        return $this->render('register.html.twig');
+    }
+
+    /**
+     * Confirm account
+     *
+     * @route /confirmation/:id-:token
+     * @method GET
+     * @return void
+     */
+    public function confirmAccount(string $id, string $token)
+    {
+        if (!$this->repository->confirmUser($id, $token)) {
+            return $this->render(
+                'register.html.twig',
+                ['error' => 'Une erreur s\'est produite lors de la validation de votre compte. Votre lien de confirmation a dû expirer.']
+            );
+        };
+
+        $this->redirect('/');
+    }
+}
