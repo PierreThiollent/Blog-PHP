030896831d32c528f1182eac9c86299026109ae8
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-04 10:33:56 +0200

    Pass categories of articles to the articles template and display it

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
+use App\Repository\CategoryRepository;
 
 class ArticlesController extends AbstractController
 {
     private ArticlesRepository $repository;
+    private CategoryRepository $categoryRepository;
 
     public function __construct()
     {
         $this->repository = new ArticlesRepository();
+        $this->categoryRepository = new CategoryRepository();
     }
 
     public function index()
     {
         $articles = $this->repository->getAll(['updatedDate' => 'DESC']);
 
-        return $this->render('articles.html.twig', ['articles' => $articles]);
+        foreach ($articles as $article) {
+            $category = $this->categoryRepository->getById($article->getCategoryId());
+            $categories[] = $category;
+        }
+
+        return $this->render('articles.html.twig', ['articles' => $articles, 'categories' => $categories]);
     }
 }
