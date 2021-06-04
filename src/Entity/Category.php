83d60235e9ab6191ae80c4e3753bb00ec12a177b
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-04 09:54:36 +0200

    Add slug property to Category entity

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ class Category
 {
     private int $id;
     private string $name;
+    private string $slug;
 
     /**
      * Getter id
@@ -46,9 +47,32 @@ class Category
      * @param  string $name
      * @return self
      */
-    public function setTitle(string $name): self
+    public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get slug.
+     *
+     * @return string|null
+     */
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set slug.
+     *
+     * @param  string $slug
+     * @return self
+     */
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
