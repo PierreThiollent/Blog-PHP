515c71f1d88b8378ea65deb658d0b5c8600472cc
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-11 10:47:21 +0200

    Add new validation method to check if property is not empty. Store errors with property_name as key

--- src/Validator.php
+++ src/Validator.php
@@ -21,7 +21,7 @@ class Validator
             foreach ($constraints as $constraint) {
                 if (method_exists($this, $constraint)) {
                     if (!is_null($this->$constraint($property_name, $data[$property_name]))) {
-                        $errors[] = $this->$constraint($property_name, $data[$property_name]);
+                        $errors[$property_name] = $this->$constraint($property_name, $data[$property_name]);
                     }
                 } else {
                     // TODO : Call logger or throw Exception
@@ -30,5 +30,18 @@ class Validator
         }
 
         return $errors;
+    }
+
+    /**
+     * Method to check if field is not empty
+     *
+     * @param $value
+     * @return string|void
+     */
+    public function isNotEmpty($value)
+    {
+        if ($value === '') {
+            return 'Le champ ne doit pas être vide.';
+        }
     }
 }
