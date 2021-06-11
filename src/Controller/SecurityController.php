c9c5cedf3dd01b2d3762f27d544f6bef47746488
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-11 12:10:31 +0200

    Create new login method into SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use App\Helpers;
 use App\Hydrator;
 use App\Repository\UserRepository;
-use App\Validator;
+use App\Validator\Validator;
 
 class SecurityController extends AbstractController
 {
@@ -68,6 +68,51 @@ class SecurityController extends AbstractController
         }
 
         return $this->render('register.html.twig');
+    }
+
+    /**
+     * Login
+     *
+     * @route /connexion
+     * @return void
+     */
+    public function login()
+    {
+        if (isset($_SESSION['user'])) {
+            return $this->redirect('/mon-compte');
+        }
+
+        if (!empty($_POST)) {
+            $user = new User();
+            $this->hydrator->hydrate($user, $_POST);
+
+            $user_data = $this->repository->userExist($user);
+
+            // Si aucun utilisateur avec cet email existe
+            if (!$user_data) {
+                return $this->render('login.html.twig', ['error' => "L'email renseigné ne correspond à aucun utilisateur"]);
+            }
+
+            // Si le compte n'a pas été confirmé
+            if (!is_null($user_data->getConfirmationToken()) && is_null($user_data->getConfirmedAt())) {
+                return $this->render(
+                    'login.html.twig',
+                    ['error' => "Votre compte n'a pas été confirmé, un email vous a été envoyé lors de la création de celui-ci, veuillez cliquer sur le lien contenu dans cet email."]
+                );
+            }
+
+            // Si le password ne match pas le hash stocké en base
+            if (!password_verify($_POST['password'], $user_data->getPassword())) {
+                return $this->render('login.html.twig', ['error' => 'Le couple email / mot de passe est incorrect.']);
+            }
+
+            // On stocke les infos du user en session
+            $_SESSION['user'] = $user_data;
+
+            return $this->redirect('/');
+        }
+
+        return $this->render('login.html.twig');
     }
 
     /**
