89cd0a39cda448ab801a850eda7eabe209116db7
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-06-25 15:04:06 +0200

    Refactor getAll and getOne methods to include author infos

--- src/Repository/ArticlesRepository.php
+++ src/Repository/ArticlesRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Article;
 use App\DAL;
 use App\Entity\Category;
+use App\Entity\User;
 use App\Hydrator;
 
 class ArticlesRepository
@@ -25,7 +26,10 @@ class ArticlesRepository
      */
     public function getAll(array $order = []): array
     {
-        $sql = 'SELECT * FROM article INNER JOIN category ON article.categoryId = category.id';
+        $sql = 'SELECT article.id, title, excerpt, content, article.slug, publishedDate, updatedDate, thumbnailUrl, name, firstname, lastname 
+                FROM article 
+                INNER JOIN category ON article.categoryId = category.id 
+                INNER JOIN user ON article.authorId = user.id';
 
         if (!empty($order)) {
             $sql .= ' ORDER BY ';
@@ -50,27 +54,39 @@ class ArticlesRepository
 
             $article['category'] = $category;
 
+            $user = new User();
+            $this->hydrator->hydrate($user, $article);
+
+            $article['author'] = $user;
+
             $article_object = new Article();
             $this->hydrator->hydrate($article_object, $article);
 
             $articles[] = $article_object;
         }
+        dump($articles);
 
         return $articles;
     }
 
     public function getOne(int $id): ?Article
     {
-        $sql = "SELECT * FROM article WHERE id = $id";
+        $sql = "SELECT article.id, title, excerpt, content, article.slug, publishedDate, updatedDate, thumbnailUrl, firstname, lastname 
+                FROM article INNER JOIN user ON article.authorId = user.id WHERE article.id = $id";
 
         $this->DAL->execute($sql);
-
         $data = $this->DAL->fetchData();
 
         if ($data) {
+            $user = new User();
+            $this->hydrator->hydrate($user, $data);
+
+            $data['author'] = $user;
+
             $article = new Article();
             $this->hydrator->hydrate($article, $data);
 
+            dump($article);
             return $article;
         }
 
