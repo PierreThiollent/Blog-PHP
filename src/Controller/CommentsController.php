9b8e4646b20e42cba2dacf5022af8f79ee754a9c
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-07-16 10:35:17 +0200

    Refactor and improve readability of code

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Hydrator;
-use App\Validator\Validator;
 use App\Repository\CommentRepository;
+use App\Validator\Validator;
 
 class CommentsController extends AbstractController
 {
@@ -21,33 +21,33 @@ class CommentsController extends AbstractController
     }
 
     /**
-     * Add comment
+     * Add comment.
      *
-     * @route /add-comment
      * @method POST ajax
-     * @return void
      */
     public function new()
     {
-        if (!empty($_POST)) {
-            $comment = new Comment();
-
-            $errors = $this->validator->validate($comment, $_POST);
-
-            if (!empty($errors)) {
-                foreach ($errors as $error) : ?>
-                    <p style="color: red;"><?= $error ?></p>
-                <?php endforeach;
-            }
-
-            $this->hydrator->hydrate($comment, $_POST);
-            $comment->setAuthor($_SESSION['user']);
-
-            if (!$this->repository->add($comment)) {
-                return $this->render('error.html.twig', ['error' => "Une erreur s'est produite pendant l'ajout de votre commentaire, veuillez réessayer."]);
-            }
-            
-            return $this->render('message.html.twig', ['message' => "Votre commentaire a bien été ajouté, il est en attente de validation."]);
+        if (empty($_POST)) {
+            return;
         }
+
+        $comment = new Comment();
+        $errors = $this->validator->validate($comment, $_POST);
+
+        if (!empty($errors)) {
+            foreach ($errors as $error) { ?>
+                <p style="color: red; margin-top: 20px;"><?php echo $error; ?></p>
+            <?php }
+            exit;
+        }
+
+        $this->hydrator->hydrate($comment, $_POST);
+        $comment->setAuthor($_SESSION['user']);
+
+        if (!$this->repository->add($comment)) {
+            return $this->render('error.html.twig', ['error' => "Une erreur s'est produite pendant l'ajout de votre commentaire, veuillez réessayer."]);
+        }
+
+        return $this->render('message.html.twig', ['message' => 'Votre commentaire a bien été ajouté, il est en attente de validation.']);
     }
 }
