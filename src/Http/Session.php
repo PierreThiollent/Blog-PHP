9ff915e731c787e59181953e5886d9107f5b2e3c
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-08-06 11:18:24 +0200

    Create Session class to manage Session

---
+++ src/Http/Session.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http;
+
+class Session
+{
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function get(string $key): mixed
+    {
+        if (array_key_exists($key, $_SESSION)) {
+            return $_SESSION[$key];
+        }
+
+        return null;
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function delete(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+}
