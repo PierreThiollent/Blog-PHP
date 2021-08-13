9731dd192476f61105aa94c89e2b2fd8f58d752d
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-08-13 15:03:11 +0200

    Fix codacy error with echo. add typing for parameter

--- src/DAL.php
+++ src/DAL.php
@@ -24,10 +24,8 @@ class DAL
             $this->db = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=' . $_ENV['DB_CHARSET'] . ';', $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
             $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
-            echo 'Erreur ! : ' . $e->getMessage();
+            throw new \PDOException($e->getMessage());
         }
-
-        return $this->isConnected();
     }
 
     /**
@@ -50,10 +48,8 @@ class DAL
 
     /**
      * Method to execute an SQL query.
-     *
-     * @param array|object $data
      */
-    public function execute(string $query, $data = []): bool
+    public function execute(string $query, object|array $data = []): bool
     {
         try {
             $request = $this->db->prepare($query);
